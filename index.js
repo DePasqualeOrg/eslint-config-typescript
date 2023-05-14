16131ef9001b8476bfadd398d3d6846e69577f5d
Anthony DePasquale <anthony@depasquale.org>
2023-05-15 00:41:04 +0200

    Allow leading underscore in variable names

--- index.js
+++ index.js
@@ -11,5 +11,21 @@ module.exports = {
     '@typescript-eslint/lines-between-class-members': ['off'],
     'import/extensions': ['off'],
     '@typescript-eslint/no-throw-literal': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'variable',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+        'leadingUnderscore': 'allow',
+      },
+      {
+        selector: 'function',
+        format: ['camelCase', 'PascalCase'],
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+    ],
   },
 };
